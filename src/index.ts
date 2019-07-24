34cde1ca4e4678d5e2e0436bb854fc775cf9c92b
rjchow <me@rjchow.com>
2019-07-24 21:25:51 +0800

    wrote directory walker

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,34 @@
+import { readdirSync } from "fs";
 import { getLogger } from "./util/logger";
 
 const { trace } = getLogger("sayHello");
 
 /**
- * This function says hello.
- * @param name Some name to say hello for.
- * @returns The hello.
+ * Walks through all files at the given path, i.e recurses into all
+ * sub directories and returns the files that exist
+ * @param path path string
  */
-const sayHello = (name: string = "Haz"): string => {
-  trace("I'm working!");
-  return `Hello, ${name}!`;
+export const traverseDirectory = (path: string): string[] => {
+  let listOfFiles: string[] = [];
+  trace(`Was called with ${path}`);
+
+  const entitiesInDirectory = readdirSync(path, { withFileTypes: true });
+  trace(`Entities:${path}: entitiesInDirectory`);
+  const subDirectories = entitiesInDirectory.filter(entity => entity.isDirectory());
+
+  const subDirectoriesPaths = subDirectories.map(subDir => subDir.name).map(dirName => [path, dirName].join("/"));
+
+  // recursion into subdirectories
+  const filesInSubdirectories = subDirectoriesPaths.map(subDirectoryPath => traverseDirectory(subDirectoryPath));
+  listOfFiles = listOfFiles.concat(...filesInSubdirectories);
+
+  const files = entitiesInDirectory.filter(entity => entity.isFile());
+  trace("Files:", files);
+
+  listOfFiles = listOfFiles.concat(files.map(file => [path, file.name].join("/")));
+
+  trace(`Current list at ${path}: ${listOfFiles}`);
+  return listOfFiles;
 };
 
-export default sayHello;
+export default traverseDirectory;
